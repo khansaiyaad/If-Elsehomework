f9e08363dc6bba9403ef3fb634479606ecf586ff
Saiyaad khan <saiyaadkhan93760@gmail.com>
2024-03-19 15:41:49 +0530

    added homework files

--- src/Leapyear.java
+++ src/Leapyear.java
@@ -7,7 +7,7 @@ public class Leapyear {
         System.out.println("Enter the year: ");
         int year = sc.nextInt();
 
-        if(year%4==0 && year%100==0 || year%400==0 ){
+        if(year%4==0 && year%100!=0 || year%400==0 ){
             System.out.println(year+" is a leap year.");
         }else {
             System.out.println(year+" is not a leap year.");
